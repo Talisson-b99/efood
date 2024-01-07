be44fb2c02949f0f184108f665faebbb2cb7750a
Talisson-b99 <talissonb422@gmail.com>
2024-01-07 05:45:37 -0300

    feat: Add ScrollToTop component for automatic page scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,25 @@ import Rotas from "./routes";
 import Footer from "./components/Footer";
 import { Globalcss } from "./styles";
 
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <>
       <BrowserRouter>
         <Globalcss />
+        <ScrollToTop />
         <Rotas />
         <Footer />
       </BrowserRouter>
